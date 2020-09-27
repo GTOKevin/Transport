11799448ba048f13e930f323c09f7bf0b300635d
GTOKevin <71615138+GTOKevin@users.noreply.github.com>
2020-09-27 14:37:13 -0500

    Add files via upload

---
+++ js/EuroMain.js
@@ -0,0 +1,12 @@
+new WOW().init();
+
+$('.logo-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true
+});
+
